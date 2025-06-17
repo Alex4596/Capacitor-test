40c5ee9bafb117d8afa63dc0fbe1c992b6e6d3eb
Alex4596 <losgary058@gmail.com>
2025-06-17 22:33:44 +0200

    First test and run

---
+++ angular-front/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app.routes';
+import {NgModule} from '@angular/core';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+})
+export class AppModule {}
